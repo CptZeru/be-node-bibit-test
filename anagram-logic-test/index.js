9f075171ae8a1bfe9db0a422531b7b8edc88c494
zerulegend <zerulegend@gmail.com>
2021-11-05 23:12:54 +0700

    feat(anagram): completed findAndSortAnagarams function

--- anagram-logic-test/index.js
+++ anagram-logic-test/index.js
@@ -1,9 +1,21 @@
 function findAndSortAnagrams(stringsArray) {
-    // Loop array
-    // Check anagram
-    // Grouping
-    // Return result
+    // Initiatlize result object
+    let resultObj = {}
+    // Loop through array of strings
+    stringsArray.forEach(str => {
+        // Sort string characters
+        const sortedStr = Array.from(str).sort().join('');
+        // Initialize object property with empty array if have not exist
+        if(!resultObj[sortedStr]) resultObj[sortedStr] = [];
+        // Anagram grouping
+        resultObj[sortedStr].push(str);
+    });
+    // Converts Object to Array and returns the Array of arrays.
+    const convertedObjArray = [];
+    Object.entries(resultObj).forEach(([key, value]) => convertedObjArray.push(value));
+    return convertedObjArray;
 }
 
 const words = ["kita", "atik", "tika", "aku", "kia", "maka", "kua"];
 console.log(words);
+console.log(findAndSortAnagrams(words));
