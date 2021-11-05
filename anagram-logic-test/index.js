8a46b15cba4a4e486f1a5eed7e9752142e37bff3
zerulegend <zerulegend@gmail.com>
2021-11-05 22:33:12 +0700

    feat(anagram): function init and define todo/flow

--- anagram-logic-test/index.js
+++ anagram-logic-test/index.js
@@ -1,2 +1,9 @@
+function findAndSortAnagrams(stringsArray) {
+    // Loop array
+    // Check anagram
+    // Grouping
+    // Return result
+}
+
 const words = ["kita", "atik", "tika", "aku", "kia", "maka", "kua"];
 console.log(words);
